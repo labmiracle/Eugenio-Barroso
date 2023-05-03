0a40c1bf2b61b9fdc51f631900b18608b73842aa
eugeAsc <eugeasc@gmail.com>
2023-05-03 18:45:02 -0300

    Ejercicio 8 Modificado

--- JavaScript/Ej-08.js
+++ JavaScript/Ej-08.js
@@ -3,4 +3,9 @@ function sumaArgs(){
     return args.reduce((total, argActual) => total + argActual, 0);
 }
 
+function sumaArgs2(...args){
+    return args.reduce((total, argActual) => total + argActual, 0);
+}
+
 console.assert(sumaArgs(0, 1, 2, 3, 4) === 10);
+console.assert(sumaArgs2(0, 1, 2, 3, 4) === 10);

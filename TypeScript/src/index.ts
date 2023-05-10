fa763fca952d77b953b7b1754603551ad4768470
eugeAsc <eugeasc@gmail.com>
2023-05-10 13:45:15 -0300

    Ejercicios 2-1 2-2 2-3 y cambios a la configs

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import { padLeft } from "./Ejercicios/Ej-2-2";
+
+//Ejercicio 2.1
+console.log('[Ejercicio 2.1]');
+
+//Ejercicio 2.2
+const ej2 = padLeft;
+console.log('[Ejercicio 2.2]', `
+${ej2('', 0)}
+${ej2('', '')}
+${ej2('', '')}
+${ej2('', '')}
+${ej2('', '')}
+`);
+
+

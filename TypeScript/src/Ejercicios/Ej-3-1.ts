e26f1bf336e68e74869420f099cf876693b01dfc
eugeAsc <eugeasc@gmail.com>
2023-05-17 13:06:12 -0300

    Ejercicio 3-1 modificado

--- TypeScript/src/Ejercicios/Ej-3-1.ts
+++ TypeScript/src/Ejercicios/Ej-3-1.ts
@@ -1,12 +1,21 @@
-function add(x: number, y: number):number {
-    return x + y;
+function add(x: number , y: number):number {
+
+    return +x + +y;
 }
     
-function sumArray(numbers: number[]):number {
-    return numbers.reduce(add, 0);
+function sumArray(numbers: (number | string)[]):number {
+    let onlyNumbers = [];
+    for(let number of numbers){
+        if (typeof number === "string") {
+            number = +number;
+            onlyNumbers.push(number);
+        }
+    }
+    return onlyNumbers.reduce(add, 0);
+    
 }
     
-const someSum = sumArray([3, 6, 9]);
+const someSum = sumArray([3, "6", 9]);
     
 
 export {someSum};

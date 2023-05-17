8ced97db54037585eb7c1f2d1220f79bc6027375
eugeAsc <eugeasc@gmail.com>
2023-05-17 13:39:55 -0300

    Ejercicio 3-3 Completo

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -3,6 +3,7 @@ import { flattenedNumbers } from "./Ejercicios/Ej-2-3";
 import { interrogateAnimal } from "./Ejercicios/Ej-2-4";
 import { someSum } from "./Ejercicios/Ej-3-1";
 import { bankAccount } from "./Ejercicios/Ej-3-2";
+import { computeScore } from "./Ejercicios/Ej-3-3";
 
 //Ejercicio 2.1
 console.log('[Ejercicio 2.1]');
@@ -28,3 +29,6 @@ console.log('[Ejercicio 3.1]', `3 + 6 + 9 === ${someSum}`);
 
 //Ejercicio 3.2
 console.log('[Exercise 3.2]', `Account value: $${bankAccount.money}`);
+
+//Ejercicio 3.3
+console.log('[Ejercicio 3.3]', `zoologico vale ${computeScore('zoo')} puntos.`);
